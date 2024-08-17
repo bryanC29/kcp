e1ada6b1028420a4d0e1ba83467b358b91dcaa16
Bryan Christy <bryanchristy2016@gmail.com>
2024-08-18 00:25:21 +0530

    RFC: Reconfigured app.jsx file for routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,24 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css'
+import Navbar from './components/navbar/navbar';
+import Index from './pages/index/index';
+import Login from './pages/login/login';
+import Courses from './pages/courses/courses';
+import Register from './pages/signup/register';
 
 function App() {
 
   return (
     <>
-      <p>Hello</p>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Index />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/courses' element={<Courses />} />
+          <Route path='/registration' element={<Register />} />
+        </Routes>
+      </Router>
     </>
   )
 }
