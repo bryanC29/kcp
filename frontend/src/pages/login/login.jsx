a7fc5fab46be66418cbade2974aa62e9b650af70
Bryan Christy <bryanchristy2016@gmail.com>
2024-08-18 00:26:18 +0530

    BUILD: Build demo pages

---
+++ frontend/src/pages/login/login.jsx
@@ -0,0 +1,9 @@
+const Login = () => {
+    return (
+        <>
+        <div>Login Hello</div>
+        </>
+    )
+}
+
+export default Login;
---
+++ frontend/src/pages/signup/register.jsx
@@ -0,0 +1,9 @@
+const Register = () => {
+    return (
+        <>
+        <div>Register Hello</div>
+        </>
+    )
+}
+
+export default Register;
