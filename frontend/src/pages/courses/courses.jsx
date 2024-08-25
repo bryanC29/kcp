e6e17c983a12541536defe245ca11655cbc8ae50
Bryan Christy <bryanchristy2016@gmail.com>
2024-08-25 14:02:27 +0530

    FEAT: Created courses dynamic page with supporting JSON data files

--- frontend/src/pages/courses/courses.jsx
+++ frontend/src/pages/courses/courses.jsx
@@ -1,7 +1,15 @@
+import CourseSection from "../../components/courseSection/courseSection";
+import courses from '../../courses/summary/courses.json';
+
 const Courses = () => {
+
     return (
         <>
-        <div>Courses Hello</div>
+        <div className="my-8 pb-8">
+            {Object.keys(courses).map((courseKey) => (
+                <CourseSection key={courseKey} courseKey={courseKey} courseTitle={courses[courseKey]} />
+            ))}
+        </div>
         </>
     )
 }
