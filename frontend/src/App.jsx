74131de4d9e98a1c4b51f51d39069930b8839dd8
Bryan Christy <bryanchristy2016@gmail.com>
2024-08-28 19:20:58 +0530

    RFC: Added dashboard route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,7 @@ import Login from './pages/login/login';
 import Courses from './pages/courses/courses';
 import Register from './pages/signup/register';
 import Course from './pages/detailedCourse/detailedCourse';
+import StudentDashboard from './pages/studentDashboard/studentDashboard';
 
 function App() {
   return (
@@ -29,6 +30,7 @@ function AppContent() {
         <Route path='/courses' element={<Courses />} />
         <Route path='/registration' element={<Register />} />
         <Route path='/courses/:courseName' element={<Course />} />
+        <Route path='/dashboard' element={<StudentDashboard />} />
       </Routes>
       {showNavbarAndFooter && <Footer />}
     </>
