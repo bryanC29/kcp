ce82ffa8d1a8b3c133c5f7a616a741855a153838
Bryan Christy <bryanchristy2016@gmail.com>
2024-08-30 17:31:52 +0530

    BUILD: Created login route

--- backend/controller/users.js
+++ backend/controller/users.js
@@ -3,24 +3,52 @@ import { uidGen } from "../utils/idGen.js";
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 
-export const login = (req, res) => {
+export const login = async (req, res) => {
     const { email, password } = req.body;
-    console.log(email, password);
-    res.status(200).json({'message': 'login done'});
+    
+    if(!email || !password)
+        return res.status(400).json({ message: 'Email and password are required' });
+
+    try {
+        const user = await Users.findOne({username: email});
+
+        if(!(user && (await bcrypt.compare(password, user.password))))
+            return res.status(400).json({ message: 'Invalid Credentials' });
+
+        const token = jwt.sign({
+            id: user._id,
+            role: user.role,
+        }, process.env.JWT_SECRET, {
+            expiresIn: '1d',
+        })
+
+        res.setHeader('Authorization', `Bearer ${token}`);
+        res.cookie('token', token, {
+            httpOnly: true,
+            maxAge: 24 * 60 * 60 * 1000,
+        })
+
+        res.status(200).json({ message: 'Login successfull' });
+    }
+
+    catch(err) {
+        console.error(err);
+        res.status(500).json({ message: 'Internal Server error while logging in' });
+    }
 }
 
 export const signup = async (req, res) => {
 
-    const { name, username, password, role = 'student' } = req.body;
+    const { name, email, password, role } = req.body;
 
-    if(!name || !username || !password)
-        return res.status(400).json({'message': 'Enter all credentials'});
+    if(!name || !email || !password)
+        return res.status(400).json({ message: 'Enter all credentials' });
 
     try{
 
-        const existingStudent = await Users.findOne({username: username});
+        const existingStudent = await Users.findOne({ username: email });
         if(existingStudent)
-            return res.status(400).json({'message': 'Failed to register. Please try again later'});
+            return res.status(400).json({ message: 'Failed to register. Please try again later' });
 
         const salt = await bcrypt.genSalt(10);
         const hash = await bcrypt.hash(password, salt);
@@ -28,9 +56,9 @@ export const signup = async (req, res) => {
 
         const newStudent = new Users({ 
             name,
-            username,
+            username: email,
             password: hash,
-            role,
+            role: (role ? role: 'student'),
             uid,
         });
         console.log(newStudent);
@@ -42,6 +70,7 @@ export const signup = async (req, res) => {
         }, process.env.JWT_SECRET, {
             expiresIn: '1d',
         });
+        console.log(token);
 
         res.setHeader('Authorization', `Bearer ${token}`);
         res.cookie('token', token, {
@@ -49,11 +78,11 @@ export const signup = async (req, res) => {
             maxAge: 24 * 60 * 60 * 1000,
         });
 
-        res.status(201).json({'message': `Successfully registered as ${newStudent.role}`});
+        res.status(201).json({ message: `Successfully registered as ${newStudent.role}` });
     }
 
     catch(err) {
         console.log(err);
-        res.status(500).json({'message': 'Internal Server error during Signup'});
+        res.status(500).json({ message: 'Internal Server error during Signup' });
     }
 }
