cf93101ae0dd280fc285968c908ddd4d78e576a9
Bryan Christy <bryanchristy2016@gmail.com>
2024-09-01 16:23:40 +0530

    RFC: Added password change routes

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,10 +1,20 @@
 import { Router } from "express";
-import { login, signup, logout } from '../controller/users.js';
+import {
+    login,
+    signup,
+    logout,
+    updatePassword,
+    passwordReset,
+    passwordResetRequest
+} from '../controller/users.js';
 
 const router = Router();
 
 router.route('/login').post(login);
 router.route('/signup').post(signup);
-router.route('/logout').get(logout)
+router.route('/logout').get(logout);
+router.route('/passwordupdate').post(updatePassword);
+router.route('/resetpassword/request').post(passwordResetRequest);
+router.route('/resetpassword/:token').get(passwordReset);
 
 export default router;
