33dd9a28cf2ec0a88cade42949c130a07b04cf91
Bryan Christy <bryanchristy2016@gmail.com>
2024-09-01 21:15:26 +0530

    RFC: Added all routes

--- backend/index.js
+++ backend/index.js
@@ -25,7 +25,21 @@ app.post('/', (req, res) => {
 })
 
 import userRoute from './routes/users.js';
+import notificationRoute from './routes/notification.js';
+import adminRoute from './routes/admin.js';
+import applicationRoute from './routes/application.js';
+import leavesRoute from './routes/leaves.js';
+import notesRoute from './routes/notes.js';
+import verificationRoute from './routes/verification.js';
+
+import tokenVerify from './middleware/tokenVerify.js';
 
 app.use('/users', userRoute);
+app.use('/notification', tokenVerify, notificationRoute);
+app.use('/admin', tokenVerify, adminRoute);
+app.use('/application', tokenVerify, applicationRoute);
+app.use('/leave', tokenVerify, leavesRoute);
+app.use('/notes', notesRoute);
+app.use('/verification', tokenVerify, verificationRoute);
 
 export default app;
